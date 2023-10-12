90ffd56e48970e10558d6afbe0dcdb3d6c966c57
Kyle <bossertkt@gmail.com>
2023-10-12 13:53:05 -0700

    Attendees list done

--- ghi/attendees/src/App.js
+++ ghi/attendees/src/App.js
@@ -1,10 +1,41 @@
 // import logo from './logo.svg';
 // import './App.css';
+import Nav from './Nav';
 
-function App() {
+function App(props) {
+  if (props.attendees === undefined) {
+    return null;
+  }
   return (
-    <div>
-    </div>
+    <>
+      <Nav />
+      <div className="container">
+        <table className="table table-striped">
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Conference</th>
+            </tr>
+          </thead>
+          <tbody>
+            {/* for (let attendee of props.attendees) {
+              <tr>
+                <td>{ attendee.name }</td>
+                <td> { attendee.conference } </td>
+              </tr>
+            } */}
+            {props.attendees.map(attendee => {
+              return (
+                <tr key = { attendee.href }>
+                  <td>{ attendee.name }</td>
+                  <td>{ attendee.conference }</td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
+    </>
   );
 }
 
