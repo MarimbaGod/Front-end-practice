0522451320ffe60acb7abd16cb9960dc637bae71
Kyle <bossertkt@gmail.com>
2023-10-12 15:43:39 -0700

    location?

--- ghi/app/src/App.js
+++ ghi/app/src/App.js
@@ -1,6 +1,9 @@
 // import logo from './logo.svg';
 // import './App.css';
 import Nav from './Nav';
+import AttendeesList from './AttendeesList';
+import LocationForm from '.LocationForm';
+
 
 function App(props) {
   if (props.attendees === undefined) {
@@ -10,33 +13,37 @@ function App(props) {
     <>
       <Nav />
       <div className="container">
-        <table className="table table-striped">
-          <thead>
-            <tr>
-              <th>Name</th>
-              <th>Conference</th>
-            </tr>
-          </thead>
-          <tbody>
-            {/* for (let attendee of props.attendees) {
-              <tr>
-                <td>{ attendee.name }</td>
-                <td> { attendee.conference } </td>
-              </tr>
-            } */}
-            {props.attendees.map(attendee => {
-              return (
-                <tr key = { attendee.href }>
-                  <td>{ attendee.name }</td>
-                  <td>{ attendee.conference }</td>
-                </tr>
-              );
-            })}
-          </tbody>
-        </table>
+        <LocationForm />
+        {/* <AttendeesList attendees={props.attendees} /> */}
       </div>
     </>
   );
 }
 
 export default App;
+
+// was between div tags
+// {/* <table className="table table-striped">
+// <thead>
+//   <tr>
+//     <th>Name</th>
+//     <th>Conference</th>
+//   </tr>
+// </thead>
+// <tbody>
+//   {/* for (let attendee of props.attendees) {
+//     <tr>
+//       <td>{ attendee.name }</td>
+//       <td> { attendee.conference } </td>
+//     </tr>
+//   } */}
+//   {props.attendees.map(attendee => {
+//     return (
+//       <tr key = { attendee.href }>
+//         <td>{ attendee.name }</td>
+//         <td>{ attendee.conference }</td>
+//       </tr>
+//     );
+//   })}
+// </tbody>
+// </table> */}
---
+++ ghi/app/src/LocationForm.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+function LocationForm () {
+    return (
+        <p>A location form</p>
+    );
+}
+export default LocationForm;
